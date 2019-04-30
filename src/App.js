c732a3473faa723000394fcb18ab1332934023b9
hideokaizuka <hideokaizuka@hideokaizuka-no-MacBook-Air.local>
2019-04-30 14:49:22 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div ><h1>Hello, world!</h1></div>
-  );
+
+// function App() {
+//   return (
+//     <div >Hello, world!</div>
+//   );
+// }
+
+class App extends Component {
+    render (){
+      return (
+        <React.Fragment>
+          <label htmlFor="bar">bar</label>
+          <input type="text" onChange={() => {console.log("I am clicked")}} />
+        </React.Fragment>
+      )
+      // const input = <input type="text" onChange={ () => {console.log("I am clicked")}} />;
+      // const greeting = "Hi!!";
+      // const dom = <h1 className="foo">{greeting} World!</h1>;
+      // return input;
+    }
 }
+
+//import Reactはjsxを使う場合は必要
+// class App extends Component {
+//   render () {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, World!!"
+//     )
+//   }
+// }
 
 export default App;
