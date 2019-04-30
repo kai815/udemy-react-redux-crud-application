5e2d9e8f9891063fb089520d8647a44dc1988307
hideokaizuka <hideokaizuka@hideokaizuka-no-MacBook-Air.local>
2019-04-30 15:23:56 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-// import React from 'react';
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
 
-
-// function App() {
-//   return (
-//     <div >Hello, world!</div>
-//   );
+// class App extends Component {
+//     render (){
+//       return (
+//         <React.Fragment>
+//           <label htmlFor="bar">bar</label>
+//           <input type="text" onChange={() => {console.log("I am clicked")}} />
+//         </React.Fragment>
+//       )
+//       // const input = <input type="text" onChange={ () => {console.log("I am clicked")}} />;
+//       // const greeting = "Hi!!";
+//       // const dom = <h1 className="foo">{greeting} World!</h1>;
+//       // return input;
+//     }
 // }
-
-class App extends Component {
-    render (){
-      return (
-        <React.Fragment>
-          <label htmlFor="bar">bar</label>
-          <input type="text" onChange={() => {console.log("I am clicked")}} />
-        </React.Fragment>
-      )
-      // const input = <input type="text" onChange={ () => {console.log("I am clicked")}} />;
-      // const greeting = "Hi!!";
-      // const dom = <h1 className="foo">{greeting} World!</h1>;
-      // return input;
-    }
-}
 
 //import Reactはjsxを使う場合は必要
 // class App extends Component {
@@ -33,5 +26,21 @@ class App extends Component {
 //     )
 //   }
 // }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+    )
+
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
 
 export default App;
